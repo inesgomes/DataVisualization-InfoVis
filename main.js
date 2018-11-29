3896e82f73a32e744c729174d851808895617f04
inesgomes <gomes.inesisabel@gmail.com>
2018-11-29 11:09:34 +0100

    start project

---
+++ main.js
@@ -0,0 +1,28 @@
+var viewWidth = window.innerWidth;
+var viewHeight = window.innerHeight;
+d3.select(window).on("resize", resize);
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40};
+var width = viewWidth - margin.left - margin.right;
+var height = viewHeight - margin.top - margin.bottom;
+
+var svg = d3.select("svg")
+    .attr("width", viewWidth)
+    .attr("height", viewHeight)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+function draw() {
+  //TODO get json and draw things!
+  console.log("draw")
+}
+
+function resize() {
+  //This function is called if the window is resized
+  //You can update your scatterplot here
+  viewWidth = window.innerWidth;
+  viewHeight = window.innerHeight;
+}
+
+
+draw();
