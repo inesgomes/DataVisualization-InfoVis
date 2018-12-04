26f6547c2bbf0ae218d05b435c67d13575c92be7
Isabel <isaneves97@gmail.com>
2018-12-04 17:09:57 +0100

    background azul sem erro nem mapa

--- main/main.js
+++ main/main.js
@@ -6,14 +6,42 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40};
 var width = viewWidth - margin.left - margin.right;
 var height = viewHeight - margin.top - margin.bottom;
 
-var svg = d3.select("svg")
+/*var svg = d3.select("svg")
     .attr("width", viewWidth)
     .attr("height", viewHeight)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+*/
 function draw() {
   //TODO get json and draw things!
+
+  var svg = d3.select("#main")
+    .attr("width", viewWidth)
+    .attr("height", viewHeight)
+    .style('background', '#C1E1EC');
+  
+  var projection = d3.geoAlbers()
+      .scale(1280)
+      .translate([viewWidth/2,viewHeight/2]);
+      //center
+
+  var  path = d3.geoPath()
+      .projection(projection);
+
+    g = svg.append("g");
+
+    d3.json('../data/europe.json', function(data) {
+      g.selectAll('.country')
+      .data(topojson.feature(data, data.objects.europe  ).features)
+      .enter()
+      .append('path')
+      .attr('class', 'country')
+      .attr('d', path);
+      return;
+  });
+
+
+  
   console.log("draw")
 }
 
