c43375c708313cd52d72ae474f14102781374813
Isabel <isaneves97@gmail.com>
2018-12-08 01:22:16 +0100

    +slider mas precisa ligeiras alteraçoes

--- main/main.js
+++ main/main.js
@@ -76,6 +76,58 @@ function drawMap(crimes, year) {
 
 }
 
+function sliderYears(){
+  var x = d3.scaleLinear()    //tem de ser discreto
+    .domain([2010, 2016])
+    .range([0, width/3])
+    .clamp(true);
+
+var slider = svg.append("g")
+    .attr("class", "slider")
+    .attr("transform", "translate(" + width/4 + "," + margin.top + ")");
+
+slider.append("line")
+    .attr("class", "track")
+    .attr("x1", x.range()[0])
+    .attr("x2", x.range()[1])
+  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-inset")
+  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-overlay")
+    .call(d3.drag()
+        .on("start.interrupt", function() { slider.interrupt(); })
+        .on("start drag", function() { hue(x.invert(d3.event.x)); }));
+
+slider.insert("g", ".track-overlay")
+    .attr("class", "ticks")
+    .attr("transform", "translate(0," + 18 + ")")
+  .selectAll("text")
+  .data(x.ticks(7))
+  .enter().append("text")
+    .attr("x", x)
+    .attr("text-anchor", "middle")
+    .text(function(d) { return d ; });
+
+var handle = slider.insert("circle", ".track-overlay")
+    .attr("class", "handle")  
+    .attr("r", 9);
+
+    
+slider.transition() // mudar para ser 'discreto'
+    .duration(750)
+    .tween("hue", function() {
+      var i = d3.interpolate(0, 70);
+      return function(t) { hue(i(t)); };
+    });
+
+function hue(h) {
+  handle.attr("cx", x(h));
+  svg.style("background-color", d3.hsl(h, 0.8, 0.8));
+}
+
+
+}
+
 function draw() {
   //get jsons
   let assaults = assault.countries;
@@ -87,6 +139,7 @@ function draw() {
 
   //TODO draw things!
   drawMap(assaults, 2010)
+  sliderYears()
   console.log("draw")
 }
 
